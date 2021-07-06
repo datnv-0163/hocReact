68f80ce8174f6ec7b79c1caf76d85040e1112c28
datnv6712 <datnv6712@gmail.com>
2021-07-06 16:22:12 +0700

    component

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+
+function Header() {
+  return (
+    
+
+    
+    <nav class="navbar navbar-inverse">
+        <a class="navbar-brand" href="#">Title</a>
+        <ul class="nav navbar-nav">
+            <li class="active">
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Link</a>
+            </li>
+        </ul>
+    </nav>
+    
+    
+  );
+}
+
+export default Header;
