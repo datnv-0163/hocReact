0240b9aaf2b0b3a707d36c661751fae12b83e58c
datnv6712 <datnv6712@gmail.com>
2021-07-07 16:56:37 +0700

    event handle and ref

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React,{Component} from 'react';
 import './App.css';
 import Header from './components/Header';
 import Product from './components/Product'
-function App() {
-  return (
+class App extends Component{
+  onAdd = ()=> {
+    console.log(this.refs.productName.value);
+    }
+  render(){
+    return(
     <div className="App">
+
+     
       <Header/>
+      
+      <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        
+
+        
+          <div className="form-group">
+            <label for="">tên</label>
+            <input type="text" class="form-control" ref="productName"/>
+          </div>
+        
+             <button type="submit" className="btn btn-primary" onClick={this.onAdd}>add</button>
+
+        
+      </div>
+      
       <Product name="iphone 8" price="5.000.0000"/>
     </div>
-  );
-}
+    );
+}}
 
 export default App;
